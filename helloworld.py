15753f19c4d5cd8244f9ebb0649b333176637be7
Filip Dostal <dostal.filip@hotmail.com>
2021-04-02 00:23:16 +0200

    init commit

---
+++ helloworld.py
@@ -0,0 +1,57 @@
+import RPi.GPIO as GPIO
+import time
+from enum import Enum
+
+
+class Diods(Enum):
+   GREEN=16
+   YELLOW=19
+   RED=21
+
+def pinCoolDown():
+    GPIO.output(21, GPIO.LOW)
+    GPIO.output(19, GPIO.LOW)
+    GPIO.output(16, GPIO.LOW)
+
+def setup():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(16, GPIO.OUT)
+    GPIO.setup(19, GPIO.OUT)
+    GPIO.setup(21, GPIO.OUT)
+
+print ("Setup")
+setup()
+
+
+def on(diod : Diods):
+    GPIO.output(diod.value, GPIO.HIGH)
+    
+def off(diod : Diods):
+    GPIO.output(diod.value, GPIO.LOW)
+
+def period(diod : Diods, lightOn : float, lightOff : float):
+    on(diod)
+    time.sleep(lightOn)
+    off(diod)
+    time.sleep(lightOff)
+    
+def periodSimple(diod : Diods, lightEqualLength : float):
+    period(diod, lightEqualLength, lightEqualLength)
+
+print ("Start")
+#while 1:
+    
+for x in range(0, 5):
+    periodSimple(Diods.GREEN, 0.05)
+    periodSimple(Diods.YELLOW, 0.1)
+    periodSimple(Diods.RED, 0.2)
+    
+for x in range(0, 5):
+    periodSimple(Diods.RED, 0.05)
+    periodSimple(Diods.YELLOW, 0.1)
+    periodSimple(Diods.GREEN, 0.2)
+
+
+pinCoolDown()
+print ("End")
